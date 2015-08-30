77b5f5fdd4417a7fe1e18f718514bca7e49f61c0
laulaua3 <loloboulet@hotmail.com>
2015-08-30 13:35:47 +0200

    Remove all vb project

--- VB/Mapper/Exception/NotSameTypePropertyException.vb
+++
@@ -1,9 +0,0 @@
-﻿Namespace Exception
-    Public Class NotSameTypePropertyException
-        Inherits System.Exception
-
-        Public Sub New(typeSource As Type, typeDest As Type)
-            MyBase.New("Les propriétés source et de destination ne sont pas du même type(source est de type " + typeSource.Name + " et destination est de type " + typeDest.Name + ")")
-        End Sub
-    End Class
-End Namespace
