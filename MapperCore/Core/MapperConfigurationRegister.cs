6cb77c7a7de696595a37242639a0ac91b49f7846
laulaua3 <loloboulet@hotmail.com>
2015-09-05 13:24:20 +0200

    Revert "Revert "First commit""
    This reverts commit 8b7eb49002fc802d2d7cd1c5c56eb99e1037aa36.

---
+++ MapperCore/Core/MapperConfigurationRegister.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapperCore.Core
+{
+    /// <summary>
+    /// Singleton des mappeurs
+    /// </summary>
+    internal class MapperConfigurationRegister : List<MapperConfigurationBase>
+    {
+
+
+        private static MapperConfigurationRegister _instance;
+
+        internal static MapperConfigurationRegister Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MapperConfigurationRegister();
+                }
+                return _instance;
+            }
+        }
+
+        private MapperConfigurationRegister()
+        {
+
+        }
+
+        internal MapperConfigurationBase Find(Type source, Type destination)
+        {
+            return this.FirstOrDefault(m => m.TypeSource == source && m.TypeDest == destination);
+        }
+    }
+}
---
+++ MapperCore/MapperExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapperCore
+{
+    public static class MapperExtentions
+    {
+        public static IEnumerable<TResult> MapTo<TSource, TResult>(this IQueryable<TSource> source)
+            where TSource : class
+            where TResult : class
+        {
+            return source.Select(Mapper.GetQueryExpression<TSource, TResult>());
+
+        }
+    }
+}
